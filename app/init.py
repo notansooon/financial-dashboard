8f62984d108a0315a807cc9ba14f979b90cb66cb
notansooon <ansonjiang043@gmail.com>
2025-04-01 02:08:29 -0400

    update

--- app/init.py
+++ app/init.py
@@ -1,7 +1,27 @@
 from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
 
-def create_app():
-    app = Flask(__name__)
+
+
+
+class Config:
+    
+    def __init__(self):
+        self.db = None
+        
+        
+        
+    def create_app(self):
+        app = Flask(__name__)
+        
+        db = SQLAlchemy(app)
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+        app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+        
+        app.config['s']
+        
+        
+        return app
            
 
 
--- app/run.py
+++ app/run.py
@@ -1,7 +1,7 @@
-from app import create_app
-from
+from app.init import Config
 
-app = create_app()
+
+app = Config().create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
